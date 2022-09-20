1953a8a2fdc0912f4ce85c6ece7f8640bd217a5f
Biz1999 <alessanbiz@gmail.com>
2022-09-20 00:53:54 -0300

    using tabulate to save diffs

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-import re
-
-from hashlib import md5
+from md5.md5 import generate_md5
 from sha256.sha256 import generate_sha256
+from tabulate import tabulate
+
 
 def main():
     print("------------------------ HASH DIFF --------------------------")
-    print("Comparando do arquivo hashes.txt...\n")
+    print("Comparando arquivo hashes.txt...\n")
 
-    sentences = []
+    md5_diffs = [["Frase", "MD5 - atual", "MD5 - gerado", "Comparação"]]
+    sha256_diffs = [["Frase", "SHA256 - atual", "SHA256 - gerado", "Comparação"]]
 
-    with open('hashes.txt') as file:
+    with open('hashes.txt', encoding="utf-8", errors="ignore") as file:
         sentences = file.readlines()
-    
-    for sentence in sentences:
-        if sentence == "\n":
-            sentences.remove(sentence)
 
     for sentence in sentences:
         substring_position = find_sub_string(sentence, "\"")
@@ -22,29 +19,32 @@ def main():
         phrase = sentence[1:substring_position].strip()
 
         hashes = sentence.replace(f"\"{phrase}\"", "").split(" - ")
-        
-        print(diff(phrase, "A primeira das instituições criadas por Pe. Roberto Sabóia de Medeiros foi a antiga Escola Superior de Administração de Negócios de São Paulo - ESAN/SP."))
 
-        # actual_md5 = hashes[-1].strip()
-        # actual_sha256 = hashes[-2].strip()
+        actual_md5 = hashes[-1].strip()
+        actual_sha256 = hashes[-2].strip()
 
-        # expected_md5 = generate_md5(phrase)
-        # expected_sha256 = generate_sha256(phrase)
+        expected_md5 = generate_md5(phrase)
+        expected_sha256 = generate_sha256(phrase)
 
-        # print(actual_md5, expected_md5)
-        # print(actual_sha256, expected_sha256)
-    
+        md5_diffs.append([phrase[:20]+"...", actual_md5, expected_md5, diff(actual_md5, expected_md5)])
+        sha256_diffs.append([phrase[:20]+"...", actual_sha256, expected_sha256, diff(actual_sha256, expected_sha256)])
+
+    md5_table = tabulate(md5_diffs, headers="firstrow", tablefmt="grid")
+    sha256_table = tabulate(sha256_diffs, headers="firstrow", tablefmt="grid")
+
+    with open("tables.txt", "w", encoding="utf-8") as tables:
+        tables.write(f"MD5 - COMPARAÇÕES\n{md5_table}\n\n")
+        tables.write(f"SHA256 - COMPARAÇÕES\n{sha256_table}")
+
+    print("Tabelas salvas!")
+
 
 def find_sub_string(sentence, string):
-	return sentence.find(string, sentence.find(string)+1)
+    return sentence.find(string, sentence.find(string) + 1)
+
 
 def diff(actual, expected):
     return actual == expected
-
-def generate_md5(phrase):
-    solution = md5(phrase.encode('utf-8')).hexdigest()
-    print(f"{phrase}, {solution}")
-    return solution
 
 
 if __name__ == "__main__":
--- md5/md5.py
+++ md5/md5.py
@@ -2,6 +2,4 @@ from hashlib import md5
 
 
 def generate_md5(phrase):
-    solution = md5(phrase.encode('utf-8')).hexdigest()
-    print(f"{phrase}, {solution}")
-    return solution
+    return md5(phrase.encode('utf-8')).hexdigest()
